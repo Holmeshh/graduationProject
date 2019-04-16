62cd6238eb117bf254c3306aa1a05e49d229110f
Holmeshh <holmeshh@163.com>
2019-04-16 18:58:16 +0800

    按实际2015-2018爬取畅销书（纸质+电子书）。保存到四个数据表中。备用

---
+++ SpiderSplit/Spider_2015/Spider_2015/pipelines.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import pymysql
+import pymysql.cursors
+import Spider_2015.settings as msg
+class Spider2015Pipeline(object):
+    def process_item(self, item, spider):
+        return item
+class DBPipeline(object):
+    def __init__(self):
+        self.connect = pymysql.connect(
+            host = msg.MYSQL_HOST,
+            db = msg.MYSQL_DBNAME,
+            user = msg.MYSQL_USER,
+            passwd = msg.MYSQL_PASSWD,
+            charset = 'utf8',
+            use_unicode = True
+        )
+        self.cursor = self.connect.cursor()
+    def process_item(self,item,spider):
+        try:
+            self.cursor.execute(
+                  """insert into 2015bookinfo(rank,book,author,press,price,comments,url)
+                value(%s,%s,%s,%s,%s,%s,%s)""",
+                (item['rank'],
+                 item['name'],
+                 item['author'],
+                 item['press'],
+                 item['price'],
+                 item['comments'],
+                 item['url']))
+            self.connect.commit()
+        except:
+            self.connect.rollback()
+        return item
+    def close_spider(self):
+        self.cursor.close()
+        self.connect.close()
---
+++ SpiderSplit/Spider_2017/Spider_2017/spiders/BookSpider.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import pymysql
+from Spider_2017.items import Spider2017Item
+import Spider_2017.settings as msg
+class get_url_list():
+	conn = pymysql.Connect(
+		host=msg.MYSQL_HOST,
+		db=msg.MYSQL_DBNAME,
+		user=msg.MYSQL_USER,
+		passwd=msg.MYSQL_PASSWD,
+		charset='utf8',
+		use_unicode=True
+	)
+	cur = conn.cursor()
+	#url_list = cur.execute("select * from paperbookurl")
+	url_list = cur.execute("SELECT * FROM paperbookurl WHERE url LIKE '%2017%' UNION SELECT * FROM ebookurl WHERE url LIKE '%2017%'")
+	url_list = list(cur.fetchall())
+	cur.close()
+	conn.close()
+class BookspiderSpider(scrapy.Spider):
+    name = 'BookSpider'
+    url_list = get_url_list.url_list
+    url_all_list = []
+    for url in url_list:
+        # print(str(url)[2:-4])#   2-77位
+        for i in range(1, 26):
+            url_all_list.append((str(url)[2:-4]) + str(i))
+    # print(url)
+    start_urls = url_all_list
+
+    def parse(self, response):
+        item = Spider2017Item()
+        book_list = response.css('ul.bang_list.clearfix.bang_list_mode').xpath('li')
+        for book in book_list:
+            item['rank'] = book.css('div.list_num').xpath('text()').extract_first()
+            item['name'] = book.css('div.name').xpath('a/text()').extract_first()
+            item['author'] = book.css('div.publisher_info')[0].xpath('a/text()').extract_first()
+            item['press'] = book.css('div.publisher_info')[1].xpath('a/text()').extract_first()
+            item['price'] = book.css('span.price_n').xpath('text()').extract_first()
+            item['comments'] = book.css('div.star').xpath('a/text()').extract_first()
+            item['url'] = book.css('div.pic').xpath('a/@href').extract_first()
+            yield item
